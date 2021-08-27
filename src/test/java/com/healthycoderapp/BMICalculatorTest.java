038f1d2ff31a52658fee494d1f01efe2be152ff4
Taylor <taylor@baremetalcloud.com>
2021-08-27 00:42:01 -0400

    Created test cases for diet recommend true and false.

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -6,9 +6,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BMICalculatorTest {
 
-
     @Test
     void should_ReturnTrue_When_DietRecommended() {
-        assertTrue(BMICalculator.isDietRecommended(89.0, 1.72));
+        // Given
+        double weight = 89.0;
+        double height = 1.72;
+
+        // When
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+
+        // Then
+        assertTrue(recommended);
+    }
+
+    @Test
+    void should_ReturnFalse_When_DietRecommended() {
+        // Given
+        double weight = 50.0;
+        double height = 1.92;
+
+        // When
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+
+        // Then
+        assertFalse(recommended);
     }
 }
