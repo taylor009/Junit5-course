4755e50755c8fdc071d1e85bd17ab7de4c041a16
Taylor <taylor@baremetalcloud.com>
2021-08-27 01:41:42 -0400

    Change @valuesource to use csv source.

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,12 +26,12 @@ public class BMICalculatorTest {
         System.out.println("After all unit tests.");
     }
 
-    @ParameterizedTest
-    @ValueSource(doubles = {89.0, 95.0, 110.0})
-    void should_ReturnTrue_When_DietRecommended(Double coderWeight) {
+    @ParameterizedTest(name = "weight={0}, height={1}")
+    @CsvSource(value = {"89.0, 1.72", "95.0, 1.75", "110.0, 1.78"})
+    void should_ReturnTrue_When_DietRecommended(Double coderWeight, Double coderHeight) {
         // Given
         double weight = coderWeight;
-        double height = 1.72;
+        double height = coderHeight;
 
         // When
         boolean recommended = BMICalculator.isDietRecommended(weight, height);
